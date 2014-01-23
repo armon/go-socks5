a56c8ea151cd2f8c87d9db6596849ed34e97052e
Armon Dadgar <armon.dadgar@gmail.com>
2014-01-23 11:29:13 -0800

    Adding request file

---
+++ request.go
@@ -0,0 +1,11 @@
+package socks5
+
+import (
+	"bufio"
+	"net"
+)
+
+// handleRequest is used for request processing after authentication
+func (s *Server) handleRequest(conn net.Conn, bufConn *bufio.Reader) error {
+	return nil
+}
